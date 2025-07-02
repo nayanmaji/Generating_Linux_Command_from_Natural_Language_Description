f855f7ef3184819147fd552cff964d3b34182a50
Nayan Maji <110929838+nayanmaji@users.noreply.github.com>
2025-07-02 15:10:44 +0530

    Add files via upload

---
+++ Android Application/app/build/generated/source/buildConfig/debug/com/example/aiassistant/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.aiassistant;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.aiassistant";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String ANTHROPIC_API_KEY = "your_anthropic_api_key_here";
+  // Field from default config.
+  public static final String GEMINI_API_KEY = "AIzaSyBt7aNiu4y6PuVusEyiev1sqFQUnoJMWQk";
+  // Field from default config.
+  public static final String OPENAI_API_KEY = "your_openai_api_key_here";
+}
